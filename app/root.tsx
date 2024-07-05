3a724971399212e812b084fd1445688b2a04e726
Will Smith <willsmithte@gmail.com>
2024-07-05 13:57:55 -0700

    add some fake env vars

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,29 @@
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
 } from "@remix-run/react";
 import "./tailwind.css";
 
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  return json({
+    ENV: {
+      GA_ID: "xyz",
+      ENV: "local",
+      GOOGLE_MAPS_KEY: "AIzab1i2312",
+      BLOG_GOOGLE_AUTH_CLIENT_ID: "something.apps.googleusercontent.com",
+      BASE_URL: "http://localhost:3333",
+    },
+  });
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -19,6 +35,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         {children}
         <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+          }}
+        />
         <Scripts />
       </body>
     </html>
